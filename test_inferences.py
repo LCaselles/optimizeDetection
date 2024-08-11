6f12ea7272ede52edbd844ddc08981c8d9a31b34
Luc Caselles <luc2.caselles@gmail.com>
2024-08-11 21:29:40 +0200

    fixes

---
+++ test_inferences.py
@@ -0,0 +1,141 @@
+import os
+import time
+import fire
+
+import torch
+import torchvision
+import numpy as np
+import cv2
+from PIL import Image
+
+import utils.detect_utils
+from metrics import compute_iou_matrix, get_scalar_metrics_with_precision_recall, parse_coordinates
+
+
+def inference_inputs(model, path_img, path_gt, dump_path, N_max=None):
+    """
+    Perform inference on input images and evaluate metrics against ground truth.
+
+    Parameters:
+    -----------
+    model : torch.nn.Module
+        The model used for inference.
+    path_img : str
+        Path to the directory containing the images.
+    path_gt : str
+        Path to the directory containing the ground truth labels.
+    dump_path : str
+        Path to save the images with predictions and ground truth bounding boxes.
+    N_max : int, optional
+        Maximum number of images to process. If None, process all images.
+
+    Returns:
+    --------
+    dict
+        A dictionary containing the computed metrics for each image.
+    """
+    metrics = {
+        "mean_iou": [],
+        "mean_max_iou_per_pred": [],
+        "mean_max_iou_per_gt": [],
+        "precision": [],
+        "recall": [],
+    }
+    os.makedirs(dump_path, exist_ok=True)
+
+    N = 0
+
+    # Read the images and run inference for detections
+    for file in os.listdir(path_img):
+        input_image = Image.open(os.path.join(path_img, file))
+        boxes, classes, labels = detect_utils.predict(input_image, model, "cpu", 0.7)
+
+        # Filter results to keep only the cars
+        len_boxes = boxes.shape[0]
+        classes_array = np.array(classes[:len_boxes], dtype=str)
+        idx_preds_car = np.where(classes_array == "car")[0]
+        boxes = boxes[idx_preds_car]
+        labels = np.array(labels[:len_boxes])[idx_preds_car]
+        classes = ["car_pred" for _ in range(len(labels))]
+
+        # Load ground truth (GT)
+        gt_boxes = parse_coordinates(os.path.join(path_gt, file.split(".png")[0] + ".txt"))
+        classes_gt = ["car_gt" for _ in range(len(gt_boxes))]
+        labels_gt = 10 * np.ones(len(gt_boxes), dtype=int)
+
+        # Dump prediction images
+        image = cv2.cvtColor(np.asarray(input_image), cv2.COLOR_BGR2RGB)
+        image = detect_utils.draw_boxes(boxes, classes, labels, image)
+        image = detect_utils.draw_boxes(np.array(gt_boxes), classes_gt, labels_gt, image)
+        save_name = file.split(".png")[0]
+        cv2.imwrite(os.path.join(dump_path, f"pred_{save_name}.jpg"), image)
+
+        # Compute metrics
+        iou_matrix = compute_iou_matrix(boxes, gt_boxes)
+        metric_img = get_scalar_metrics_with_precision_recall(iou_matrix, 0.65)
+        for key in metric_img:
+            metrics[key].append(metric_img[key])
+
+        N += 1
+        if N_max is not None and N >= N_max:
+            break
+
+    return metrics
+
+
+def main(path_img, path_gt, dump_path_quantized="predictions_quantized", 
+         dump_path_non_quantized="predictions", 
+         path_quantized_model='quantized_model_full.pth', 
+         path_non_quantized_model="non_quantized_model_full.pth"):
+    """
+    Main function to run inference with and without quantization, and print metrics.
+
+    Parameters:
+    -----------
+    path_img : str
+        Path to the directory containing the images.
+    path_gt : str
+        Path to the directory containing the ground truth labels.
+    dump_path_quantized : str, optional
+        Path to save the images for quantized model predictions.
+    dump_path_non_quantized : str, optional
+        Path to save the images for non-quantized model predictions.
+    path_quantized_model : str, optional
+        Path to the quantized model file.
+    path_non_quantized_model : str, optional
+        Path to the non-quantized model file.
+    """
+    # Load the models
+    model_quantized = torch.load(path_quantized_model)
+    model_quantized.eval()
+
+    model_non_quantized = torchvision.models.detection.fasterrcnn_mobilenet_v3_large_fpn(pretrained=True)
+    model_non_quantized.eval()
+
+    # Inference with quantized model
+    print("Start inference with quantization")
+    start = time.time()
+    metrics = inference_inputs(model_quantized, path_img, path_gt, dump_path_quantized, N_max=15)
+    print("Time to compute:", time.time() - start)
+    print("Quantized metrics:")
+    for metric in metrics.keys():
+        print(metric, np.mean(metrics[metric]))
+
+    # Inference with non-quantized model
+    print("Start inference without quantization")
+    start = time.time()
+    metrics = inference_inputs(model_non_quantized, path_img, path_gt, dump_path_non_quantized, N_max=15)
+    print("Time to compute:", time.time() - start)
+    print("Non-quantized metrics:")
+    for metric in metrics.keys():
+        print(metric, np.mean(metrics[metric]))
+
+"""
+if __name__ == "__main__":
+    # Define paths (these can be changed to command-line arguments or configuration files)
+    path_img = "dataset/dataset/images/"
+    path_gt = "dataset/dataset/labels/"
+    main(path_img, path_gt)
+"""
+if __name__ == "__main__":
+    fire.Fire(main)
--- utils/detect_utils.py
+++ utils/detect_utils.py
@@ -1,45 +1,81 @@
-import torchvision
-import torch
-import argparse
-import cv2
-from PIL import Image
-import torchvision.transforms as transforms
 import cv2
 import numpy as np
+import torch
+import torchvision
+import torchvision.transforms as transforms
+from PIL import Image
 from coco_names import COCO_INSTANCE_CATEGORY_NAMES as coco_names
 
-# this will help us create a different color for each class
+# Random colors for each class for visualization
 COLORS = np.random.uniform(0, 255, size=(len(coco_names), 3))
-# define the torchvision image transforms
+
+# Define image transformations
 transform = transforms.Compose([
     transforms.ToTensor(),
 ])
 
 def draw_boxes(boxes, classes, labels, image):
+    """
+    Draw bounding boxes and class labels on the image.
+
+    Args:
+        boxes (np.ndarray): Array of bounding boxes.
+        classes (list): List of class names.
+        labels (torch.Tensor): Tensor of class labels.
+        image (np.ndarray): The image to draw on.
+
+    Returns:
+        np.ndarray: The image with bounding boxes and labels drawn.
+    """
     for i, box in enumerate(boxes):
         color = COLORS[labels[i]]
         cv2.rectangle(
             image,
             (int(box[0]), int(box[1])),
             (int(box[2]), int(box[3])),
-            color, 2
+            color.tolist(),  # Convert color to list for cv2
+            2
         )
-        cv2.putText(image, classes[i], (int(box[0]), int(box[1]-5)),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.8, color, 2, 
-                    lineType=cv2.LINE_AA)
+        cv2.putText(
+            image,
+            classes[i],
+            (int(box[0]), int(box[1] - 5)),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            0.8,
+            color.tolist(),  # Convert color to list for cv2
+            2,
+            lineType=cv2.LINE_AA
+        )
     return image
 
 def predict(image, model, device, detection_threshold):
-    # transform the image to tensor
-    image = transform(image).to(device)
-    image = image.unsqueeze(0) # add a batch dimension
-    outputs = model(image) # get the predictions on the image
-    # get all the predicited class names
+    """
+    Predict bounding boxes and class labels for the given image.
+
+    Args:
+        image (PIL.Image.Image or np.ndarray): The input image.
+        model (torch.nn.Module): The object detection model.
+        device (torch.device): The device to run the model on.
+        detection_threshold (float): Score threshold for detections.
+
+    Returns:
+        tuple: A tuple containing:
+            - boxes (np.ndarray): Array of bounding boxes.
+            - pred_classes (list): List of predicted class names.
+            - labels (torch.Tensor): Tensor of predicted class labels.
+    """
+    # Convert image to tensor and add batch dimension
+    image = transform(image).to(device).unsqueeze(0)
+    
+    # Get model predictions
+    outputs = model(image)
+    
+    # Extract class names, scores, and bounding boxes
     pred_classes = [coco_names[i] for i in outputs[0]['labels'].cpu().numpy()]
-    # get score for all the predicted objects
     pred_scores = outputs[0]['scores'].detach().cpu().numpy()
-    # get all the predicted bounding boxes
     pred_bboxes = outputs[0]['boxes'].detach().cpu().numpy()
-    # get boxes above the threshold score
+    
+    # Filter boxes by detection threshold
     boxes = pred_bboxes[pred_scores >= detection_threshold].astype(np.int32)
+    
     return boxes, pred_classes, outputs[0]['labels']
